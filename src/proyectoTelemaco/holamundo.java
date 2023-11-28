d5491597fc4e4edfcd2633800b9cb6328cd996f3
vfernandezr01 <vfernandezr01@educarex.es>
2023-11-28 09:10:26 +0100

    inicio Proyecto

---
+++ src/proyectoTelemaco/holamundo.java
@@ -0,0 +1,12 @@
+package proyectoTelemaco;
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hola chavales");
+
+	}
+
+}
